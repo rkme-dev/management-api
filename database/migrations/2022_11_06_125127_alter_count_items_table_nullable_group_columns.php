8af11602e0572caec05d043a2ed5da8caf729d39
jonathanpdicen <dicenjonathanp@gmail.com>
2022-11-06 14:54:22 +0800

    feat: nullable group columns

---
+++ database/migrations/2022_11_06_125127_alter_count_items_table_nullable_group_columns.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('count_items', function (Blueprint $table) {
+            $table->string('group_1')->nullable()->change();
+            $table->string('group_2')->nullable()->change();
+        });
+    }
+
+    public function down()
+    {
+        $table->string('group_1')->nullable(false)->change();
+        $table->string('group_2')->nullable(false)->change();
+    }
+};
