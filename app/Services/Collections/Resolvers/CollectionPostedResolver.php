a0578719dac467972e7215b0d52323a4cc426380
rkme-dev <enco.developer.01@gmail.com>
2022-11-09 21:56:34 +0800

    - Fixed collection posting

--- app/Services/Collections/Resolvers/CollectionPostedResolver.php
+++ app/Services/Collections/Resolvers/CollectionPostedResolver.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace App\Services\Collections\Resolvers;
 
+use App\Jobs\Collection\SalesDrPaymentPostingJob;
 use App\Models\Collection;
-use App\Jobs\Collection\ProductInitializeInventoryJob;
 use App\Services\Collections\Interfaces\CollectionPostedResolverInterface;
 
 final class CollectionPostedResolver implements CollectionPostedResolverInterface
@@ -15,7 +15,7 @@ final class CollectionPostedResolver implements CollectionPostedResolverInterfac
         $salesDrPayments = $collection->salesDrPayments;
 
         foreach ($salesDrPayments as $salesDrPayment) {
-            ProductInitializeInventoryJob::dispatch($salesDrPayment->getAttribute('id'));
+            SalesDrPaymentPostingJob::dispatch($salesDrPayment->getAttribute('id'));
         }
     }
 }
