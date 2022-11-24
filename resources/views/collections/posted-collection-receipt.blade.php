35adeea63ebc2035a7f06d030e452cd62c9ffefb
Terrence Tejada <terrence.tejada@lfuggoc.net>
2022-11-24 10:03:25 +0800

    posted collection receipt report adjustment

--- resources/views/collections/posted-collection-receipt.blade.php
+++ resources/views/collections/posted-collection-receipt.blade.php
@@ -7,6 +7,12 @@
     body {
         font-family: 'Roboto Condensed', sans-serif;
     }
+    .title {
+        color: #0070c0;
+    }
+    .font-i {
+        font-style: italic;
+    }
 
     .m-0 {
         margin: 0px;
@@ -19,41 +25,84 @@
     .pt-5 {
         padding-top: 5px;
     }
-
-    .p-10 {
-        padding: 10px;
+    .pt-50 {
+        padding-top: 50px;
+    }
+    .pt-130 {
+        padding-top: 130px;
     }
 
-    .p-20 {
-        padding: 20px;
+    .pd-10 {
+        padding-bottom: 10px;
     }
-
-    .p-30 {
-        padding: 20px;
+    .pl-7 {
+        padding-left: 7px;
+    }
+    .pl-40 {
+        padding-left: 40px;
+    }
+    .pr-40 {
+        padding-right: 40px;
     }
 
     .mt-10 {
         margin-top: 10px;
     }
+    .mt-50 {
+        margin-top: 50px;
+    }
 
     .text-center {
         text-align: center !important;
+    }
+    .text-right {
+        text-align: right !important;
     }
 
     .w-100 {
         width: 100%;
     }
-
+    .w-40 {
+        width: 40%;
+    }
     .w-50 {
         width: 50%;
+    }
+    .w-60 {
+        width: 60%;
+    }
+    .w-80 {
+        width: 80%;
     }
 
     .w-85 {
         width: 85%;
     }
 
+    .w-10 {
+        width: 10%;
+    }
     .w-15 {
         width: 15%;
+    }
+    .w-20 {
+        width: 20%;
+    }
+    .w-25 {
+        width: 20%;
+    }
+    .w-28 {
+        width: 28%;
+    }
+    .w-30 {
+        width: 30%;
+    }
+    .font-9 {
+        font-size: 9;
+    }
+
+    .font-10 {
+        font-size: 10;
     }
 
     .logo img {
@@ -121,37 +170,92 @@
     .total-right p {
         padding-right: 20px;
     }
+    .copy {
+        font-size: 15px !important;
+        font-weight: lighter;
+        font-style: italic;
+        margin-top: 10px;
+        margin-right: 30px;
+        position: absolute;
+        top: 118px;
+        right: 0px;
+    }
+    .inline{
+        display: inline-block;
+    }
+    .nextline{
+        display: block;
+    }
+    .bd-none {
+        border-left: none;
+        border-bottom: none;
+        border-right: none;
+    }
+    .footer {
+        position: absolute;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+    }
+    .page-break {
+        page-break-after: always;
+    }
+    
 </style>
 <body>
 
 @include('includes.header')
-{{-- <div class="mt-10 text-bold w-50" style="position:absolute; left: 20px;top: -10px;">
-    <img src="../public/enco.png" height="60px" alt="Image"/>
-</div> --}}
 <div class="head-title">
-    <h2 class="text-center mt-1 p-0">COLLECTION RECEIPT</h2>
+    <h2 class="text-center mt-50">COLLECTION RECEIPT</h2>
 </div>
-<div class="add-detail mt-10">
-    <div class="w-50 float-left mt-10">
-        <p class="m-0 pt-5 text-bold w-100">Customer Name: <span class="gray-color">{{$order['customer']['name']}}</span></p>
-        <p class="m-0 pt-5 text-bold w-100">Address: <span class="gray-color">{{$order['customer']['address']}}</span></p>
+
+<div class="mt-10 font-10">
+    <div class="nextline">
+        <div class="inline w-50">
+            <p class="m-0 pt-5 w-100 pd-10"><span class="text-bold">Customer Name:</span> <span class="gray-color"> {{$order['customer']['name']}} </span></p>
+        </div>
+        <div class="inline w-30">
+            <p class="m-0 pt-5 w-100 pd-10"><span class="text-bold">CR No:</span> <span class="gray-color">{{$order['collection_order_number']}}</span></p>
+        </div>
     </div>
-    <div class="w-50 float-right mt-10">
-        <p class="m-0 pt-5 text-bold w-100">CR No: <span class="gray-color">{{$order['collection_order_number']}}</span></p>
-        <p class="m-0 pt-5 text-bold w-100">Status: <span class="gray-color">{{$order['status']}}</span></p>
+    <div class="nextline">
+        <div class="inline w-50">
+            <p class="m-0 pt-5 w-100 pd-10"><span class="text-bold">Address:</span> <span class="gray-color"> {{$order['customer']['address']}} </span></p>
+        </div>
+        <div class="inline w-30">
+            <p class="m-0 pt-5 w-100 pd-10"><span class="text-bold">Status:</span> <span class="gray-color"> {{$order['status']}} </span></p>
+        </div>
     </div>
+    
     <div style="clear: both;"></div>
 </div>
 
-<div class="add-detail mt-10">
-    <div class="w-50 float-left mt-10">
-        <p class="m-0 pt-5 text-bold w-100">Remarks: </p>
+<div class="mt-10 font-10">
+    <div class="nextline">
+        <div class="inline w-50">
+            <p class="m-0 pt-5 w-100 pd-10"><span class="text-bold">Remarks:</span>  <span class="gray-color">{{strtoupper(str_replace('_',' ', $order['remarks']))}}</span></p>
+        </div>
+        <div class="inline w-30">
+            <p class="m-0 pt-5 w-100 pd-10"><span class="text-bold">Total Amount Due</span> <span class="gray-color"> Php {{ $order['amount'] }} </span></p>
+        </div>
     </div>
-    <div class="w-50 float-right mt-10">
-        <p class="m-0 pt-5 text-bold w-100">Total Amount Due: <span class="gray-color">Php {{ $order['amount'] }}</span></p>
-        <p class="m-0 pt-5 text-bold w-100">Total Amount Collected: <span class="gray-color">Php {{ $order['amount_collected'] }}</span></p>
-        <p class="m-0 pt-5 text-bold w-100">Balance: <span class="gray-color">Php {{ $order['balance'] }}</span></p>
+    <div class="nextline">
+        <div class="inline w-50">
+            {{-- <p class="m-0 pt-5 w-100 pd-10"><span class="text-bold">Total Amound Collected:</span> <span class="gray-color"> </span></p> --}}
+        </div>
+        <div class="inline w-30">
+            <p class="m-0 pt-5 w-100 pd-10"><span class="text-bold">Total Collected:</span> <span class="gray-color"> Php {{ $order['amount_collected'] }} </span></p>
+        </div>
     </div>
+    <div class="nextline">
+        <div class="inline w-50">
+            {{-- <p class="m-0 pt-5 w-100 pd-10"><span class="text-bold">Total Amound Collected:</span> <span class="gray-color"> </span></p> --}}
+        </div>
+        <div class="inline w-30">
+            <p class="m-0 pt-5 w-100 pd-10"><span class="text-bold">Balance:</span> <span class="gray-color"> Php {{ $order['balance'] }} </span></p>
+        </div>
+    </div>
+    
     <div style="clear: both;"></div>
 </div>
 
@@ -200,17 +304,13 @@
 </div>
 
 
-<div class="table-section bill-tbl w-100 mt-10">
+
+
+<div class="table-section bill-tbl w-100 pt-130">
     <table class="table w-100 mt-10">
-        <tr>
-            <th class="w-50">Acctg Prepared:</th>
-            <th class="w-50">Received By:</th>
-            <div style="clear: both;"></div>
-        </tr>
-        <tr align="center">
-            <td class="p-30"></td>
-            <td class="p-30"></td>
-            <div style="clear: both;"></div>
+        <tr  style="border-style: none" align="center">
+            <td class="p-30" style="border-style: none">Acctg Prepared:</td>
+            <td class="p-30" style="border-style: none">Received By:</td>
         </tr>
     </table>
 </div>
