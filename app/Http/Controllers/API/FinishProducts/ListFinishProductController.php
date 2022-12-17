546cefae4801e59e7410f85955964729f060485f
rkme-dev <enco.developer.01@gmail.com>
2022-12-17 16:35:56 +0800

    - update finish product list endpoint

--- app/Http/Controllers/API/FinishProducts/ListFinishProductController.php
+++ app/Http/Controllers/API/FinishProducts/ListFinishProductController.php
@@ -13,10 +13,30 @@ final class ListFinishProductController extends AbstractAPIController
 {
     public function __invoke(): JsonResource
     {
-        return new JsonResource(Product::where('type', ProductTypeEnums::FINISHED_PRODUCT->value)
+        $finishedProducts = Product::where('type', ProductTypeEnums::FINISHED_PRODUCT->value)
             ->whereNull('deleted_at')
-        ->with(['units', 'rawMaterials.rawMaterial'])
+            ->with(['units', 'rawMaterials.rawMaterial'])
             ->orderBy('created_at', 'desc')
-            ->get());
+            ->get();
+
+        $results = [];
+
+        foreach ($finishedProducts as $finishedProduct) {
+            $computed = $finishedProduct->toArray();
+
+            if (count($finishedProduct->rawMaterials ?? []) > 0) {
+                $computedRawMaterials = [];
+
+                foreach ($finishedProduct->rawMaterials as $rawMaterial) {
+                    $computedRawMaterials[] = $rawMaterial->rawMaterial;
+                }
+
+                $computed['raw_materials'] = $computedRawMaterials;
+            }
+
+            $results[] = $computed;
+        }
+
+        return new JsonResource($results);
     }
 }
